08989643b0e53d6f01eae38efa344c5817b312b9
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2025-07-29 21:16:46 +0300

    fix: hides /graphql from docs(swagger)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from strawberry.fastapi import GraphQLRouter
-
 # app import
 from app.routers import items
 from app.graphql.schema import schema
 
 app = FastAPI()
+graphql_app = GraphQLRouter(schema)
 
 app.include_router(items.router)
-graphql_app = GraphQLRouter(schema)
+
+for route in graphql_app.routes:
+    if isinstance(route, APIRoute):
+        route.include_in_schema = False
+
 app.include_router(graphql_app, prefix="/graphql")
